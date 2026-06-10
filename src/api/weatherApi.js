056cf8201651038e1d946d0416651effc5e8ab1e
annasun919 <yuval_sun@yahoo.com.hk>
2026-06-11 00:34:44 +0800

    edit fetch error

--- src/api/weatherApi.js
+++ src/api/weatherApi.js
@@ -1,4 +1,8 @@
 const BASE = 'http://localhost:3000/api/weather';
 
 export const fetchSummary = () =>
-  fetch(`${BASE}/summary`).then(r => r.json());
+  fetch(`${BASE}/summary`)
+    .then(res => {
+      if (!res.ok) throw new Error(`Server error: ${res.status}`);
+      return res.json();
+    });
